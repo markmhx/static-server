86d57ea4677682f4345f113a121d124f501e5ddd
Mark Hendrickson <markmhendrickson@gmail.com>
2018-03-26 13:52:23 +0200

    Integrate grunt-hoist for deployment

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+/**
+ * Configure Grunt scripts
+ * @module
+ */
+
+require('park-ranger')();
+var loadGruntTasks = require('load-grunt-tasks');
+
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig();
+  loadGruntTasks(grunt);
+};
--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.use(express.static(process.env.STATIC_SERVER_ASSETS_DIR));
 
 let port = process.env.STATIC_SERVER_ASSETS_PORT ? process.env.STATIC_SERVER_ASSETS_PORT : 80;
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log(`Static server is running on ${port}`);
 });
