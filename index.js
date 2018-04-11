66954c2177fd2fcae40cef3980599178ecaf5a6b
Mark Hendrickson <markmhendrickson@gmail.com>
2018-04-11 10:41:31 +0200

    Fix STATIC_SERVER_PORT env variable name

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express'),
 
 app.use(express.static(process.env.STATIC_SERVER_ASSETS_DIR));
 
-let port = process.env.STATIC_SERVER_ASSETS_PORT ? process.env.STATIC_SERVER_ASSETS_PORT : 80;
+let port = process.env.STATIC_SERVER_PORT ? process.env.STATIC_SERVER_PORT : 80;
 
 app.listen(port, () => {
   console.log(`Static server is running on ${port}`);
